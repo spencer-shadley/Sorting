c22f4ca49a38153993e8c5cf0dd59ebf2a973640
Spencer Shadley <spencershadley@utexas.edu>
2019-07-07 16:21:53 -0700

    Added more tests

--- TestSort/TestSort.cs
+++ TestSort/TestSort.cs
@@ -1,3 +1,4 @@
+using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Sort;
 
@@ -8,8 +9,37 @@ namespace TestSort
         public static ISort[] Sorts = new ISort[] {new DefaultArraySort()};
 
         [TestMethod]
-        public void TestSort_Simple() {
+        public void TestSort_Simple()
+        {
             VerifySort(input: new[] { 1, 9, 5, 3, 2 }, expected: new int[] { 1, 2, 3, 5, 9 });
+        }
+
+        [TestMethod]
+        public void TestSort_Reverse()
+        {
+            VerifySort(input: new[] { 5,4,3,2,1,0 }, expected: new int[] { 0,1,2,3,4,5 });
+        }
+
+        [TestMethod]
+        public void TestSort_Duplicates()
+        {
+            VerifySort(input: new[] { 1, 1, 5, 1, 2 }, expected: new int[] { 1, 1, 1, 2, 5 });
+        }
+
+        [TestMethod]
+        public void TestSort_Fuzz() {
+            const uint arrSize = 100;
+            const int randomRange = 10;
+
+            var random = new Random();
+            var input = new int[arrSize];
+            for (int i = 0; i < arrSize; ++i) {
+                input[i] = random.Next(minValue: -randomRange, maxValue: randomRange);
+            }
+
+            var arr = (int[])input.Clone();
+            Array.Sort(input);
+            VerifySort(input: arr, expected: input);
         }
 
         public static void VerifySort(int[] input, int[] expected) {
