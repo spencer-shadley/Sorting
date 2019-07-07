6c2e7188c5cb378ff78459947a9aa5570387664d
Spencer Shadley <spencershadley@utexas.edu>
2019-07-07 16:29:01 -0700

    Added more tests

--- TestSort/TestSort.cs
+++ TestSort/TestSort.cs
@@ -27,6 +27,36 @@ namespace TestSort
         }
 
         [TestMethod]
+        public void TestSort_Sorted()
+        {
+            VerifySort(input: new[] { 1, 2, 3, 4, 5 }, expected: new int[] { 1, 2, 3, 4, 5 });
+        }
+
+        [TestMethod]
+        public void TestSort_Negatives_All()
+        {
+            VerifySort(input: new[] { -1, -9, -5, -3, -2 }, expected: new int[] { -9, -5, -3, -2, -1 });
+        }
+
+        [TestMethod]
+        public void TestSort_Negatives_Mixed()
+        {
+            VerifySort(input: new[] { -1, 9, 0, -5, 3, 2 }, expected: new int[] { -5, -1, 0, 2, 3, 9 });
+        }
+
+        [TestMethod]
+        public void TestSort_Zeroes_All()
+        {
+            VerifySort(input: new[] { 0, 0, 0 }, expected: new int[] { 0, 0, 0 });
+        }
+
+        [TestMethod]
+        public void TestSort_Zeroes_Mixed()
+        {
+            VerifySort(input: new[] { 0, 8, 0 }, expected: new int[] { 0, 0, 8 });
+        }
+
+        [TestMethod]
         public void TestSort_Fuzz_Small()
         {
             VerifyFuzz(10, 10);
